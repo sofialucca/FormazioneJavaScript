eca2b79e0c6077ffb80f30f93bb1e0472bb33e0a
Sofia <sofy99@fastwebnet.it>
2021-04-22 15:48:22 +0200

    Avanced Objects

---
+++ Advanced Object/arrow_functions_this.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const robot = {
+    energyLevel: 100,
+    checkEnergy: function() {
+      console.log(`Energy is currently at ${this.energyLevel}%.`)
+    }
+  }
+  
+  robot.checkEnergy();
---
+++ Advanced Object/factory_functions.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const robotFactory= (model,mobile)=>{
+    return {
+      model:model,
+      mobile:mobile,
+      beep:()=>{
+        console.log('Beep Boop');
+      },
+    }
+  }
+  
+  const tinCan=robotFactory('P-500',true);
+  
+  tinCan.beep();
