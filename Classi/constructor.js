861bc5ed90a32ed149ba4644f075d62428304704
Sofia <sofy99@fastwebnet.it>
2021-04-23 12:20:20 +0200

    Classi

---
+++ Classi/constructor.js
@@ -0,0 +1,8 @@
+'use strict';
+
+class Surgeon{
+    constructor(name,department){
+      this._name=name;
+      this._department=department;
+    }
+  }
---
+++ Classi/instance.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class Surgeon {
+    constructor(name, department) {
+      this.name = name;
+      this.department = department;
+    }
+  }
+  
+  const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
+  
+  const surgeonJackson = new Surgeon("Ruth Jackson", "Orthopedics");
